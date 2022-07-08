3d864da4aa8c9fc628d9fef581a5558f6035538d
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2022-07-08 13:47:00 +0000

    fix hmr in ladle

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 var config = {
-  server: {
-    open: 'none',
-  }
+      server: {
+        hmr: {
+				clientPort: process.env.HMR_HOST ? 443: 24678,
+				host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
+			}
+      },
 }
 
 export default config;
