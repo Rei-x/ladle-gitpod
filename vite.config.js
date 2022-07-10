42d5a964584621a88866c7f08739460730d8905f
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2022-07-10 10:47:38 +0000

    dont open the server

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 var config = {
       server: {
+        open: "none",
         hmr: {
 				clientPort: process.env.HMR_HOST ? 443: 24678,
 				host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
